ce1e22d864d7c0f2bc0cd15a6e3ac14da3109ced
Bailey <baileymteply@gmail.com>
2021-09-03 17:21:31 -0500

    downloaded node packages

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+})
+
+app.listen(3001, () => {
+    console.log('hello from port 3001')
+})
