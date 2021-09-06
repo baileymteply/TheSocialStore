8c5328221ae308eaa830ba82e329d956690366f1
baileymteply <baileymteply@gmail.com>
2021-09-06 12:24:58 -0500

    another test push:

--- src/components/SpecialComps/AllProducts.js
+++ src/components/SpecialComps/AllProducts.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import '../index.css'
 
 function AllProducts() {
+
+    const [productList, setProductList] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/api/get')
+        .then((response) => {
+            setProductList(response.data)
+        })
+    })
+
     return (
-        <div>
-            
-            
+        <div className="product-container">
+            {productList.map((val) => {
+                return (
+                    <h1>
+                        productName: {val.productName}, productPrice: {val.productPrice}
+                    </h1>
+                )
+            })}
         </div>
     )
 }
